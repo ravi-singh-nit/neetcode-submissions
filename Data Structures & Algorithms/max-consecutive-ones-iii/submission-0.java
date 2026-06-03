911e4dafec5fb7ae82804437d57266dadb76eeda
ravi-singh-nit <31826118+ravi-singh-nit@users.noreply.github.com>
2026-06-03 13:23:07 +0530

    Add: max-consecutive-ones-iii - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l=0;
+        int n=nums.length;
+        int ans =0;
+        int count =0;
+        for(int r=0;r<n;r++){
+            if(nums[r]==0)
+            count++;
+            while(count>k){
+                if(nums[l++]==0){
+                    count--;
+                }
+            }
+            ans=Math.max(r-l+1,ans);
+        }
+        return ans;
+        
+    }
+}
